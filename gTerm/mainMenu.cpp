909f2158875765f3abb8941d355425548a09bc8c
outgoingbot <outgoingbot@gmail.com>
2026-05-09 23:54:07 -0500

    changed how show debug works in main menu

--- gTerm/mainMenu.cpp
+++ gTerm/mainMenu.cpp
@@ -15,9 +15,6 @@ void mainMenu::update() {
 
     if (ImGui::BeginMenu("File")) { // Add a "File" menu
 
-        if (ImGui::MenuItem("New")) {
-            // Handle "New" action        
-        }
         if (ImGui::MenuItem("Open")) {
             // Handle "Open" action
             cfgManagerP.ShowOpenDiag();
@@ -73,32 +70,33 @@ void mainMenu::update() {
 
         ImGui::Separator();
 
-        if (!turbo_mode) {
-            ImGui::SliderFloat("Min FPS (experimental)", &frame_rate_slider_val, frame_rate_min_val, frame_rate_max_val, "%.0f");
-
-            if (ImGui::IsItemDeactivatedAfterEdit()) {
-                //do something
-            }
-        }
-
-        if (ImGui::Checkbox("Show Ball", &show_ball)) {
-            //something else of needed
-        }
-
-        if (ImGui::Checkbox("Show Debug", &show_debug)) {
-            //something else of needed
-        }
-
-        ImGui::Separator();
-
         ImGui::SliderFloat("Font Size", &currentFontSize, 8.0f, 60.0f, "%.0f px");
         if (ImGui::IsItemDeactivatedAfterEdit())        // alternative (more precise)
         {
             fontNeedsRebuild = true;
         }
 
+
+        if (ImGui::Checkbox("Show Debug", &show_debug)) {
+            //something else of needed
+        }
+
+
+
         if (show_debug) {
             ImGui::Separator();
+            
+            if (ImGui::Checkbox("Show Ball", &show_ball)) {
+                //something else of needed
+            }
+            if (!turbo_mode) {
+                ImGui::SliderFloat("Min FPS (experimental)", &frame_rate_slider_val, frame_rate_min_val, frame_rate_max_val, "%.0f");
+
+                if (ImGui::IsItemDeactivatedAfterEdit()) {
+                    //do something
+                }
+            }
+            ImGui::Text("Theme Colors:");
             ImGui::SliderFloat("red", &globalColors.red, 0.0f, 1.0f, "%.2f");
             ImGui::SliderFloat("green", &globalColors.green, 0.0f, 1.0f, "%.2f");
             ImGui::SliderFloat("blue", &globalColors.blue, 0.0f, 1.0f, "%.2f");
